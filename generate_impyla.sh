288341b88b349e86af91a3d03e001fed64948580
Matt Hollingsworth <matt@deepfield.net>
2014-07-24 14:13:15 -0400

    Update generate impyla

--- generate_impyla.sh
+++ generate_impyla.sh
@@ -2,23 +2,25 @@
 
 IMPYLA_REPO=`pwd`
 OUTPUT=$IMPYLA_REPO/impala/thrift
+ARGS="-gen py:new_style -out $OUTPUT"
+ARGS="-gen py:new_style"
 
 # generate the python code
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/Data.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/DataSinks.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/Descriptors.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/Exprs.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/Frontend.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/ImpalaInternalService.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/ImpalaService.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/NetworkTest.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/Partitions.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/PlanNodes.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/Planner.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/RuntimeProfile.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/StateStoreService.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/Status.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/Types.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/beeswax.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/cli_service.thrift
-thrift -gen py:new_style -out $OUTPUT $IMPYLA_REPO/thrift/parquet.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/Data.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/DataSinks.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/Descriptors.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/Exprs.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/Frontend.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/ImpalaInternalService.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/ImpalaService.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/NetworkTest.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/Partitions.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/PlanNodes.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/Planner.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/RuntimeProfile.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/StateStoreService.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/Status.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/Types.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/beeswax.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/cli_service.thrift
+thrift $ARGS $IMPYLA_REPO/thrift/parquet.thrift
