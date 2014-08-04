13298a18a156229acb2b4375bef8be8f23139186
Matthew Hollingsworth <mr.hworth@gmail.com>
2014-08-04 01:29:38 -0400

    Bring query out to global var

--- src/impala/tests/test_run.py
+++ src/impala/tests/test_run.py
@@ -2,12 +2,13 @@
 import impala.rpc_beeswax as rpc
 from impala.dbapi import connect
 
+TEST_QUERY = "SELECT * FROM sometable"
 def print_row(row):
     print '\t'.join([str(v) for v in row])
 
 def test_rpc():
     service = rpc.connect_to_impala('localhost')
-    query_handle = rpc.execute_statement(service, "SELECT * FROM deepfield_drill_small_step300.drill_small_2014_07_14t14_30", user='support')
+    query_handle = rpc.execute_statement(service, TEST_QUERY, user='support')
     schema = rpc.get_result_schema(service, query_handle)
     max_rows = 1024
     rows = rpc.fetch_results(service, query_handle, schema=schema, max_rows=max_rows)
@@ -23,7 +24,7 @@ def test_rpc():
 def test_dbapi():
     connection = connect(port=21000, rpc_impl='hs1')
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM deepfield_drill_small_step300.drill_small_2014_07_14t14_30 LIMIT 10')
+    cursor.execute(TEST_QUERY)
     print cursor.fetchall()
 
 test_dbapi()
